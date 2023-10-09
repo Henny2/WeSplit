11ef68b5a301b0762f415f04cfbf9f79e2213ed1
Henny2 <henrieke.baunack@web.de>
2023-10-08 19:15:55 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Henrieke Baunack on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
