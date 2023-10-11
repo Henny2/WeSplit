26557bc8ec816ab19996d58e2e95e8f7406e6620
Henny2 <henrieke.baunack@web.de>
2023-10-10 20:35:23 -0700

    adding picker for tip amount

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    let tipPercentages = [0, 10, 15, 20, 25]
+
+    
     var body: some View {
         NavigationStack{
             Form{
@@ -24,6 +27,14 @@ struct ContentView: View {
                        
                     }
                     // .pickerStyle(.navigationLink) if you want to change the style of the picker
+                }
+                Section("How much tip do you want to add?"){
+                    Picker("Tip Percentage Picker", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
                 }
                 Section{
                     Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
