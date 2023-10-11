dd2158755abde74c90819dddd683990694d675e8
Henny2 <henrieke.baunack@web.de>
2023-10-10 20:22:15 -0700

    adding picker and input for wesplit apps main inputs - amount and number of people

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationStack{
+            Form{
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                       
+                    }
+                    // .pickerStyle(.navigationLink) if you want to change the style of the picker
+                }
+                Section{
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }.navigationTitle("WeSplit")
+        }
     }
 }
 
